e0d950ee6eaa54d3d8688253417a95abcd6a3933
Rinchyenbat Erdenebaatar <rinchyenbaterdenebaatar@Ri-3.local>
2025-12-23 01:09:43 +0800

    admin1

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -3,7 +3,7 @@ import { User } from '../models/User.js';
 export async function listUsers(req, res) {
   const me = req.user.id;
 
-  const users = await User.find({ _id: { $ne: me } })
+  const users = await User.find({ _id: { $ne: me }, role: { $ne: 'admin' } })
     .select({ username: 1 })
     .sort({ username: 1 })
     .lean();
